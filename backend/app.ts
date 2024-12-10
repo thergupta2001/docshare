e4ab146fa2c5aa621d47d3435a2423929a190722
thergupta2001 <2001.rohangupta@gmail.com>
2024-12-10 21:06:14 +0530

    project setup

---
+++ backend/app.ts
@@ -0,0 +1,7 @@
+import express, {Express, Request, Response} from 'express';
+
+const app : Express = express();
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080 + ts +gdfhbdgd");
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className='text-4xl'>Hello</p>
+    </>
+  )
+}
+
+export default App
